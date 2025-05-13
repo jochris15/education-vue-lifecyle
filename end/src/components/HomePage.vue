63a10cd2252ef09139c22c34fdc4c4b7366ce1cf
jochris15 <jochris15@gmail.com>
2025-05-13 10:07:23 +0700

    add loading

--- end/src/components/HomePage.vue
+++ end/src/components/HomePage.vue
@@ -13,15 +13,19 @@ import {
 } from "vue";
 
 const products = ref([]);
+const loading = ref(false);
 
 async function fetchProducts(url) {
   try {
+    loading.value = true;
     const response = await fetch(url);
     const data = await response.json();
 
     products.value = data.products;
   } catch (error) {
     console.error("Error fetching products:", error);
+  } finally {
+    loading.value = false;
   }
 }
 
@@ -71,8 +75,12 @@ onUnmounted(() => {
 </script>
 
 <template>
+  <div class="flex flex-col items-center justify-center h-screen" v-if="loading">
+    <img src="../assets/Bean Eater@1x-1.0s-200px-200px.svg" class="w-1/3" />
+  </div>
+
   <!-- product list -->
-  <div id="PAGE-HOME" class="min-h-screen flex items-center justify-center">
+  <div id="PAGE-HOME" class="min-h-screen flex items-center justify-center" v-else>
     <main class="my-5 bg-white grid grid-cols-4 gap-5">
       <div
         class="h-full flex flex-col justify-center items-center bg-yellow-400 border-2 border-black p-5 rounded-2xl shadow-[2px_2px_0px_rgba(0,0,0,1)]"
